4ab9198fe4fb0f450ef77b16369a9d3cd4fde0e7
Chuck D'Antonio <c_dantonio@acm.org>
2017-12-06 15:54:46 -0800

    Initial cut at compliance capabilities

---
+++ ci/tasks/compliance-docs.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env bash
+
+inputDir=  outputDir=  artifactId=  packaging=
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    -o | --output-dir )
+      outputDir=$2
+      shift
+      ;;
+    -a | --artifactId )
+      artifactId=$2
+      shift
+      ;;
+    -p | --packaging )
+      packaging=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+
+if [ ! -d "$inputDir" ]; then
+  echo "missing input directory!"
+  exit 1
+fi
+
+if [ ! -d "$outputDir" ]; then
+  echo "missing output directory!"
+  exit 1
+fi
+
+artifactName="${artifactId}-compliance-${version}.${packaging}"
+
+compliance-masonry get
+compliance-masonry docs gitbook LATO
+
+tar -C ${inputDir}/exports -czf ../$outputDir/$artifactName *
+
+ls ${outputDir}
