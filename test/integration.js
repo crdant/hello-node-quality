458c5beb28b479a67839998d854b7525df25d83a
Chuck D'Antonio <c_dantonio@acm.org>
2017-12-06 16:43:02 -0800

    Ooops. We test first for a reason.

--- test/integration.js
+++ test/integration.js
@@ -18,7 +18,7 @@ axios.get(url, { httpsAgent: agent })
     console.log("Got response: " + response.status);
     console.log("Got data: " + response.data);
 
-    if ( response.data != "Hello World\n") {
+    if ( response.data != "Hello Quality! We are compliant.\n") {
       process.exit(-1);
     }
   })
